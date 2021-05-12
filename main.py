ca198dbb96cbb3c76edc84415b512b9c9428e2af
Iron512 <ivanmartini96@gmail.com>
2021-05-12 06:47:52 +0000

    background clensing. time for background subtraction

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ class bcolors:
 	RESET = '\033[0m'
 
 #check if an argument is passed
-if len(sys.argv) != 2:	
-	print("\nError found:" + bcolors.RED + " Incorrect arguments. " + bcolors.RESET + "\nTerminating.\n\nRun using this command:\n\n\t" + bcolors.GREEN + "python3 main.py <video_file>\n" + bcolors.RESET)
+if len(sys.argv) != 3:	
+	print("\nError found:" + bcolors.RED + " Incorrect arguments. " + bcolors.RESET + "\nTerminating.\n\nRun using this command:\n\n\t" + bcolors.GREEN + "python3 main.py <video_file> <type>\n" + bcolors.RESET)
 	sys.exit(1)
 
 #check if the argument passed exist as file
 video = sys.argv[1]
+typeof = sys.argv[2]
+
 if not os.path.exists(video):
 	print("\nError found:" + bcolors.RED + " invalid file. " + bcolors.RESET + "\nTerminating.\n\nArgument passed ("+ bcolors.YELLOW + video + bcolors.RESET +") is not a valid file.\n")
 	sys.exit(2)
+
+if typeof != "mode" and typeof != "average":
+	print("\nError found:" + bcolors.RED + " invalid type. " + bcolors.RESET + "\nTerminating.\n\nArgument passed ("+ bcolors.YELLOW + typeof + bcolors.RESET +") is not a valid type.\nUse" + bcolors.YELLOW + "average" + bcolors.RESET + " or " + bcolors.YELLOW + "mode" + bcolors.RESET + " instead.")
+	sys.exit(2)	
 
 #check if the argument passed is a valid video
 cap = cv2.VideoCapture(video)
@@ -32,7 +38,6 @@ if not cap.isOpened():
 width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
 
-frames = []
 frames_gray = []
 i = 0
 
@@ -45,13 +50,14 @@ while True:
 
 		cv2.imshow('frame',frame)
 		cv2.imshow('gray',gray)
-		cv2.waitKey(30)
+		cv2.waitKey(1)
 
 		i=i+1
 	else:
 		break
 
-average_test = np.zeros((height,width))
+#if typeof == "mode":
+#average_test = np.zeros((height,width))
 mode_test = np.zeros((height,width),dtype=object)
 mode_result = np.zeros((height,width))
 old_percentage = -1
@@ -61,7 +67,7 @@ for x in range(height):
 		count = 0
 		mode_test[x][y] = []
 		for k in range(len(frames_gray)):
-			average_test[x][y] = average_test[x][y] + frames_gray[k][x][y]
+			#average_test[x][y] = average_test[x][y] + frames_gray[k][x][y]
 			mode_test[x][y].append(frames_gray[k][x][y])
 			count = count+1			
 
@@ -72,7 +78,7 @@ for x in range(height):
 old_percentage = -1
 for x in range(height):
 	for y in range(width):
-		average_test[x][y] = average_test[x][y]/count
+		#average_test[x][y] = average_test[x][y]/count
 		mode_result[x][y] = max(mode_test[x][y], key=mode_test[x][y].count)
 
 	if old_percentage != int((x+1)*100/height):
@@ -82,9 +88,9 @@ for x in range(height):
 
 print("analyzed frames:",count)
 
-cv2.imshow('average', average_test.astype(np.uint8))
+#cv2.imshow('average', average_test.astype(np.uint8))
 cv2.imshow('moded', mode_result.astype(np.uint8))
-cv2.imwrite('average.png', average_test.astype(np.uint8))
+#cv2.imwrite('average.png', average_test.astype(np.uint8))
 cv2.imwrite('mode.png', mode_result.astype(np.uint8))
 
 cv2.waitKey(0)
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+from pathlib import Path
+
+import cv2
+import sys
+import os
+import numpy as np
+
+import utility as ut
+
+#This file contains 3 function, for preparing the video to further processes.
+#As described in the report, this setup phase can be executed just once, in order
+#to save time.
+
+#LOADCAPTURE: verifies the integrity of the video passed as parameter, 
+#opening and returning the videocapture object (if the operation was successful)
+def loadCapture(videopath):
+	#check if the argument passed exist as file
+	if not os.path.exists(videopath):
+		print("\nError found:", ut.red("invalid file."), "\nTerminating.\n\nArgument passed ("+ ut.ylw(videopath) +") is not a valid file.\n")
+		return False, None
+
+	#check if the argument passed is a valid video
+	cap = cv2.VideoCapture(videopath)
+	if not cap.isOpened():
+		print("Error found:",ut.red("wrongly formatted file (Not a video)."), "\nTerminating.\n\nCheck again the file passed ("+ ut.ylw(videopath)+").\n")
+		return False, None
+
+	return True, cap
+
+#SPLITCAPTURE: Waiting each time for the video to be loaded can be tedious as
+#the framerate is costant. The video gets splitted and saved as images, in order to
+#allow the loading of just these.
+def splitCapture(capture, step = 1, write = False, waiting_time = 1):
+	if write:
+		Path("./images").mkdir(parents=True, exist_ok=True)
+	video = []
+
+	i = 0
+	while True:
+		ret, frame = capture.read()
+		if ret:
+			if i % step == 0:
+				video.append(frame)
+
+			cv2.imshow('frame', frame)
+			if write:
+				cv2.imwrite("./images/colored"+str(i)+".png", frame)
+			cv2.waitKey(waiting_time)
+
+			i=i+1
+		else:
+			break
+
+	return video
+
+#LOADIMAGES: Once split, the video must be loaded back into memory. 
+#I honestly thought this step would reduce a lot the loading phase,
+#but turns out that it is still worth to load the video completely
+def loadImages():
+	qt = len([name for name in os.listdir('./images/')])
+	
+	video = [None] * qt
+	old_percentage = -1
+	for i in range(qt):
+		frame = cv2.imread("./images/colored"+str(i)+".png",1)
+		video[i] = frame
+		if old_percentage != int((i+1)*100/qt):
+			old_percentage = int((i+1)*100/qt)
+			print("Images loaded:", ut.grn(str(old_percentage) + "%"))
+
+	return video
